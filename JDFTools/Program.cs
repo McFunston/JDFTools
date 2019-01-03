7ab08969854f1de8c791520a6aaf8fe0522f0dd1
Mica Funston <mica.funston@gmail.com>
2019-01-02 19:12:53 -0500

    Initial

---
+++ JDFTools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JDFTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
