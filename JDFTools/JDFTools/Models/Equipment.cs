3f5a238e6c3ec3b4f0d97b55905a9c16f566269c
Mica Funston <mica.funston@gmail.com>
2019-03-29 20:06:20 -0400

    Add Equipment class.

---
+++ JDFTools/JDFTools/Models/Equipment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JDFTools.Models
+{
+    class Equipment
+    {
+        public string Name { get; set; }
+        public bool Perfecting { get; set; }
+        public string Type { get; set; }
+        public float PlateWidth { get; set; }
+        public float PlateHeight { get; set; }
+    }
+}
